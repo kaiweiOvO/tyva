29031568e638eeb57142d8ded8034dfd159c19cd
kaiweiovo <54873477+kaiweiOvO@users.noreply.github.com>
2022-04-26 15:33:52 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,125 +1,114 @@
-import { AllTypeOptions, Type, TypeOptions } from "./types";
+import { AllTypeOptions, Type, TypeOptions, ValueType } from "./types";
 
-export default function vaildateType<T>(
-  value: T,
-  type: T extends object
-    ? T extends Iterable<any>
-      ? AllTypeOptions
-      : { [k in keyof T]: AllTypeOptions }
-    : AllTypeOptions
-): void {
-  console.log(value, type);
-  if (typeof value === "object" && Array.isArray(value) === false) {
-    // value type is object
-    // object | array | TypeOptions
-    if (typeof type === "string") {
-      // error object value called type string
-      console.log(`\x1B[31munexpected object type ${type}. \x1B[0m`);
-    } else {
-      // type.keys
-      const keys = Object.keys(type as { [k in keyof T]: AllTypeOptions });
+export default function vaildateType<T>(value: T, type: ValueType<T>, key?: string): void {
+  if (typeof value === 'object' && Array.isArray(value) === false) {
+      // value type is object
+      // object | array | TypeOptions
+      if (typeof type === 'string') {
+          // error object value called type string
+          checkType(value, type, key);
+      } else {
+          // type.keys
+          const keys = Object.keys(type as { [k in keyof T]: AllTypeOptions<T> });
 
-      keys.forEach((key) => {
-        const parameter = (value as any)[key];
+          keys.forEach((k) => {
+              const parameter = (value as any)[k];
 
-        if (parameter === undefined) {
-          // unmatched parameter
-          console.log(`\x1B[31munmatched parameter ${key}. \x1B[0m`);
-        }
-        const option = (type as { [k: string]: TypeOptions })[key];
+              const option = (type as { [k: string]: TypeOptions<typeof parameter> })[k];
 
-        if (typeof option === "object") {
-          checkTypeOption(parameter, option, key);
-        } else {
-          checkType(parameter, option);
-        }
-      });
-    }
-  } else {
-    // value type is Type
-    // string | number | bool | undefined | TypeOptions
-    if (typeof type === "string") {
-      // Type
-      checkType(value, type);
-    } else {
-      // TypeOptions
-      checkTypeOption(value, type as TypeOptions);
-    }
-  }
-}
-
-/**
- * check value type
- * @param {any} value
- * @param {Type} type
- */
-function checkType(value: unknown, type: Type) {
-  if (type === "array" && Array.isArray(value) === false) {
-    // value === array
-    console.log(
-      `\x1B[31mexpected type is ${type}, but received ${typeof value}. \x1B[0m`
-    );
-    return;
-  } else if (typeof value !== type) {
-    if (type === "array") return;
-    // value === string | number | bool | undefined
-    console.log(
-      `\x1B[31mexpected type is ${type}, but received ${typeof value}. \x1B[0m`
-    );
-  }
-}
-
-/**
- * check value type options
- * @param {any} value
- * @param {TypeOptions} option
- */
-function checkTypeOption(value: any, option: TypeOptions, key?: string) {
-  switch (option.type) {
-    case "string":
-      // validate string type option
-      validateString(
-        value,
-        option.length,
-        option.maxLen,
-        option.minLen,
-        option.format
-      );
-      break;
-    case "number":
-      // validate number type option
-      validateNumber(value, option.float, option.max, option.min);
-      break;
-    case "object":
-      // todo check object
-      break;
-    case "array":
-      // validate array type option
-      validateArray(
-        value,
-        option.length,
-        option.maxLen,
-        option.minLen,
-        option.eachValue
-      );
-      break;
-    // other type option
-    case "boolean":
-    case "undefined":
-      if (typeof value !== option.type) {
-        console.log(
-          `\x1B[31m${key ? key + " " : ""}expected type is ${
-            option.type
-          }, but received ${typeof value}. \x1B[0m`
-        );
+              if (typeof option === 'object') {
+                  checkTypeOption(parameter, option, k);
+              } else {
+                  checkType(parameter, option, k);
+              }
+          });
       }
-      break;
-    default:
-      console.log(
-        `\x1B[31m${key ? key + " " : ""}expected type is ${
-          option.type
-        }, but received ${typeof value}. \x1B[0m`
-      );
+  } else {
+      // value type is Type
+      // string | number | bool | undefined | TypeOptions
+      if (typeof type === 'string') {
+          // Type
+          checkType(value, type, key);
+      } else {
+          // TypeOptions
+          checkTypeOption(value, type as TypeOptions<typeof value>, key);
+      }
+  }
+}
+
+/**
+* check value type
+* @param {any} value
+* @param {Type} type
+*/
+function checkType(value: unknown, type: Type, key?: string) {
+  if (type === 'array' && Array.isArray(value) === false) {
+      // value === array
+      throw new Error(`${key ? `parameter ${key} ` : ''}expected type is ${type}, but received ${typeof value}.`);
+  } else if (Array.isArray(value) === true && type !== 'array') {
+      // value === string | number | bool | object | undefined
+      throw new Error(`${key ? `parameter ${key} ` : ''}expected type is ${type}, but received array.`);
+      // eslint-disable-next-line valid-typeof
+  } else if (Array.isArray(value) === false && typeof value !== type) {
+      // value === string | number | bool | object | undefined
+      throw new Error(`${key ? `parameter ${key} ` : ''}expected type is ${type}, but received ${typeof value}.`);
+  }
+}
+
+/**
+* check value type options
+* @param {any} value
+* @param {TypeOptions} option
+*/
+function checkTypeOption(value: any, option: TypeOptions<any>, key?: string) {
+  switch (option.type) {
+      case 'string':
+          // validate string type option
+          validateString(value, option.length, option.maxLen, option.minLen, option.format, key);
+          break;
+      case 'number':
+          // validate number type option
+          validateNumber(value, option.float, option.max, option.min, key);
+          break;
+      case 'object':
+          // todo check object
+          if (option.values) {
+              const keys = Object.keys(option.values);
+              keys.forEach((k) => {
+                  const parameter = (value as any)[k];
+
+                  const op = (option.values as { [k: string]: TypeOptions<typeof option.values> })[k];
+
+                  if (typeof op === 'object') {
+                      checkTypeOption(parameter, op, k);
+                  } else {
+                      checkType(parameter, op, k);
+                  }
+              });
+          } else {
+              checkType(value, 'object', key);
+          }
+          break;
+      case 'array':
+          // validate array type option
+          validateArray(value, option.length, option.maxLen, option.minLen, option.each, key);
+          break;
+      // other type option
+      case 'boolean':
+          checkType(value, 'boolean', key);
+          break;
+      case 'undefined':
+          // eslint-disable-next-line valid-typeof
+          if (typeof value !== option.type) {
+              throw new Error(
+                  `${key ? `parameter ${key} ` : ''}expected type is ${option.type}, but received ${typeof value}.`
+              );
+          }
+          break;
+      default:
+          throw new Error(
+              `${key ? `parameter ${key} ` : ''}expected type is ${option.type}, but received ${typeof value}.`
+          );
   }
 }
 
@@ -128,50 +117,40 @@ function validateString(
   length?: number,
   maxLen?: number,
   minLen?: number,
-  format?: RegExp
+  format?: RegExp,
+  key?: string
 ) {
-  checkType(value, "string");
+  checkType(value, 'string', key);
   if (length !== undefined && value.length !== length) {
-    console.log(
-      `\x1B[31mexpected length ${length}, got value length ${value.length}. \x1B[0m`
-    );
+      throw new Error(
+          `${key ? `parameter ${key} ` : ''}expected length ${length}, got value length ${value.length}.`
+      );
   }
   if (maxLen !== undefined && value.length > maxLen) {
-    console.log(
-      `\x1B[31mexpected maximum length ${maxLen}, got value length ${value.length}. \x1B[0m`
-    );
+      throw new Error(
+          `${key ? `parameter ${key} ` : ''}expected maximum length ${maxLen}, got value length ${value.length}.`
+      );
   }
   if (minLen !== undefined && value.length < minLen) {
-    console.log(
-      `\x1B[31mexpected minimum length ${minLen}, got value length ${value.length}. \x1B[0m`
-    );
+      throw new Error(
+          `${key ? `parameter ${key} ` : ''}expected minimum length ${minLen}, got value length ${value.length}.`
+      );
   }
   if (format !== undefined && format.test(value) === false) {
-    console.log(
-      `\x1B[31mexpected format ${format}, got mismatched value ${value}. \x1B[0m`
-    );
+      throw new Error(`${key ? `parameter ${key} ` : ''}expected format ${format}, got mismatched value ${value}.`);
   }
 }
 
-function validateNumber(
-  value: number,
-  float?: boolean,
-  max?: number,
-  min?: number
-) {
-  checkType(value, "number");
-  if (float !== undefined && ~~value === value) {
-    console.log(`\x1B[31mvalue ${value} is not fload number. \x1B[0m`);
+function validateNumber(value: number, float?: boolean, max?: number, min?: number, key?: string) {
+  checkType(value, 'number', key);
+  if (float === true && value % 1 === 0) {
+      throw new Error(`${key ? `parameter ${key} ` : ''}value ${value} is not fload number.`);
   }
   if (max !== undefined && value > max) {
-    console.log(
-      `\x1B[31mvalue ${value} is bigger than maximum value ${max}. \x1B[0m`
-    );
+      throw new Error(`${key ? `parameter ${key} ` : ''}value ${value} is bigger than maximum value ${max}.`);
   }
   if (min !== undefined && value < min) {
-    console.log(
-      `\x1B[31mvalue ${value} is smaller than minimum value ${min}. \x1B[0m`
-    );
+      throw new Error(`${key ? `parameter ${key} ` : ''}value ${value} is smaller than minimum value ${min}.`);
   }
 }
 
@@ -180,27 +159,32 @@ function validateArray(
   length?: number,
   maxLen?: number,
   minLen?: number,
-  eachValue?: TypeOptions
+  each?: AllTypeOptions<unknown>,
+  key?: string
 ) {
-  checkType(value, "array");
+  checkType(value, 'array', key);
   if (length !== undefined && value.length !== length) {
-    console.log(
-      `\x1B[31mexpected length ${length}, got value length ${value.length}. \x1B[0m`
-    );
+      throw new Error(
+          `${key ? `parameter ${key} ` : ''}expected length ${length}, got value length ${value.length}.`
+      );
   }
   if (maxLen !== undefined && value.length > maxLen) {
-    console.log(
-      `\x1B[31mexpected max length ${maxLen}, got value length ${value.length}. \x1B[0m`
-    );
+      throw new Error(
+          `${key ? `parameter ${key} ` : ''}expected maximum length ${maxLen}, got value length ${value.length}.`
+      );
   }
   if (minLen !== undefined && value.length < minLen) {
-    console.log(
-      `\x1B[31mexpected min length ${minLen}, got value length ${value.length}. \x1B[0m`
-    );
+      throw new Error(
+          `${key ? `parameter ${key} ` : ''}expected minimum length ${minLen}, got value length ${value.length}.`
+      );
   }
-  if (eachValue !== undefined) {
-    value.forEach((v) => {
-      vaildateType(v, eachValue);
-    });
+  if (each !== undefined) {
+      value.forEach((v) => {
+          if (typeof each === 'string') {
+              checkType(v, each, key);
+          } else {
+              vaildateType(v, each, key);
+          }
+      });
   }
 }
